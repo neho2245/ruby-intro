50d8e953fab9d8835a1d2e9cdf62dc10ade28900
neho <alexneho@yahoo.com>
2026-04-04 16:36:16 +0300

    Made some code improvements

--- caesar-cypher/caesara.rb
+++ caesar-cypher/caesara.rb
@@ -1,48 +1,16 @@
-END_UPPERCASE = 91;
-END_LOWERCASE = 123;
-LETTERS = 26;
-
-def wrap(character, wrap_start)
-    return (wrap_start - LETTERS) + (character % wrap_start) if character >= wrap_start
-    character
-end
-
-def isupcase?(character)
-  character == character.upcase
-end
-
-def isletter?(character)
-  if (character.ord > (END_LOWERCASE - LETTERS - 1) and character.ord < END_LOWERCASE)
-    return true
-  end
-  if (character.ord > (END_UPPERCASE - LETTERS - 1) and character.ord < END_UPPERCASE)
-    return true
-  end
-  return false
-end
-
 def caesar_cypher(message, shift_amount)
-    split_encoded_message = message.split('').map do |character| 
-        if (isletter?(character))
-          encoded_character = character.ord + shift_amount
-          if (isupcase?(character))
-            wrap(encoded_character, END_UPPERCASE).chr
-          else
-            wrap(encoded_character, END_LOWERCASE).chr
-          end
-        else
-            character
-        end
-    end
+    shift_amount = shift_amount % 26
 
-    # merge the encoded message back into a single one
-    split_encoded_message.reduce('') { | new_string, character | new_string << character}
+    message.chars.map do |char|
+        if (char.between?('a', 'z'))
+            ((char.ord - 'a'.ord + shift_amount) % 26 + 'a'.ord).chr
+        elsif (char.between?('A', 'Z'))
+            ((char.ord - 'A'.ord + shift_amount) % 26 + 'A'.ord).chr
+        else
+            char
+        end
+    end.join
+
 end
 
-# p('A'.ord())
-# p(caesar_cypher("w", 5))
-p(caesar_cypher("What a string!", 5))
-# p(65.chr)
-# p('w'.ord)
-# p((123 - LETTERS).chr)
-# p(wrap(65, 91));
+p(caesar_cypher("string", 25));
