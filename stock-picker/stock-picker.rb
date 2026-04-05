b87f03e11da836658d066429bf15feb542118408
neho <alexneho@yahoo.com>
2026-04-05 09:51:25 +0300

    Modified the stock-picker function
    It now returns a pair of days instead of just the profit

--- stock-picker/stock-picker.rb
+++ stock-picker/stock-picker.rb
@@ -5,16 +5,21 @@ def stock_picker (stock_prices)
   left = 0
   right = 1
   max_profit = 0
+  days = []
 
   while right < stock_prices.length - 1
-    max_profit = [max_profit, stock_prices[right] - stock_prices[left]].max
+    profit = stock_prices[right] - stock_prices[left]
+    if (profit > max_profit)
+      max_profit = profit
+      days = [left,  right]
+    end
     if (stock_prices[right] < stock_prices[left])
       left = right
     end
     right += 1
   end
 
-  return max_profit
+  return days
 end
 
 p(stock_picker(prices))
